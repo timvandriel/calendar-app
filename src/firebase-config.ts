2f1104a76ee1ef89d8941d98fbf26bedebb46156
Tim Van Driel <timothy.vandriel@gmail.com>
2025-03-10 09:48:17 -0600

    make env and firebase config to hide APIsecrets

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,15 +1,14 @@
-
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSfGF9m_GZT0bM5oOPw05zQRqECitJD7E",
-    authDomain: "fnof-stack-5884c.firebaseapp.com",
-    projectId: "fnof-stack-5884c",
-    storageBucket: "fnof-stack-5884c.firebasestorage.app",
-    messagingSenderId: "865707808577",
-    appId: "1:865707808577:web:af78f07f724efed9fd73c0",
-    measurementId: "G-VDP6CE0ETR"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
